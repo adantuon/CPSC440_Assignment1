04158dbd42a847a3e017d7b2a99fc5638bb0182a
adantuon <adantuon@mail.umw.edu>
2025-05-24 18:00:15 -0400

    Added the timer

--- CPSC440_Assignment1.cpp
+++ CPSC440_Assignment1.cpp
@@ -16,11 +16,32 @@ int main()
 	thread1 = al_create_thread(timer, NULL);
 	thread2 = al_create_thread(playerInput, NULL);
 
+	while (!gameComplete && !timerComplete) {
+		if (!gameComplete && !timerComplete) {
+			al_start_thread(thread1);
 
+			al_start_thread(thread2);
+		}
+		else {
+			al_destroy_thread(thread1);
+
+			al_destroy_thread(thread2);
+		}
+	}
 }
 
 void *timer(ALLEGRO_THREAD *ptr, void *arg) {
-	return(0);
+	time_t startTime, currentTime;
+	startTime = time(NULL);
+	currentTime = time(NULL);
+
+	//TO DO: Change this to 60 seconds
+	while (currentTime - startTime < 5 && !gameComplete) {
+		currentTime = time(NULL);
+	}
+	gameComplete = true;
+
+	return(NULL);
 }
 
 void *playerInput(ALLEGRO_THREAD *ptr, void *arg) {
