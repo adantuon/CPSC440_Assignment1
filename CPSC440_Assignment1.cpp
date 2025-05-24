0d9f4e5e4095d3111f6603a93a5310f0dc7f0128
adantuon <adantuon@mail.umw.edu>
2025-05-24 17:00:59 -0400

    Set up threads

--- CPSC440_Assignment1.cpp
+++ CPSC440_Assignment1.cpp
@@ -2,7 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void *timer(ALLEGRO_THREAD *ptr, void *arg);
+
+void *playerInput(ALLEGRO_THREAD *ptr, void *arg);
+
+bool gameComplete = false;
+bool timerComplete = false;
+
 int main()
 {
+	ALLEGRO_THREAD *thread1 = NULL, *thread2 = NULL;
 
+	thread1 = al_create_thread(timer, NULL);
+	thread2 = al_create_thread(playerInput, NULL);
+
+
+}
+
+void *timer(ALLEGRO_THREAD *ptr, void *arg) {
+	return(0);
+}
+
+void *playerInput(ALLEGRO_THREAD *ptr, void *arg) {
+	return(0);
 }
